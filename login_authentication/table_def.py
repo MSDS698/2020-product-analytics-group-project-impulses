da5ffae4fbe92cfd9a065de25992671661132d18
andrewhowcool <andrewcyoung1@gmail.com>
2020-03-24 23:16:34 -0700

    simple front end and not working backend

---
+++ login_authentication/table_def.py
@@ -0,0 +1,25 @@
+from sqlalchemy import *
+from sqlalchemy import create_engine, ForeignKey
+from sqlalchemy import Column, Date, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
+
+engine = create_engine('sqlite:///login.db', echo=True)
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String)
+    password = Column(String)
+
+
+def __init__(self, username, password):
+    self.username = username
+    self.password = password
+
+
+# create tables
+Base.metadata.create_all(engine)
