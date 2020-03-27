631ffb015bbebfc1393daff50e97015efc688e40
esme <esmeluo@gmail.com>
2020-03-27 00:10:57 -0700

    terminate environment delete app/pipeline, change html font

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
-# Create environment
+# Create environment and shutdown everything in the end
 echo "3\n\nImpulses_script\nY\n1\nn\nn\n" | eb init -i
 eb create imp-env-test
 aws codepipeline create-pipeline --cli-input-json file://pipeline.json
 eb terminate
-#aws elasticbeanstalk delete-application --application-name Impulses_script
+aws elasticbeanstalk delete-application --application-name Impulses_script
+aws codepipeline delete-pipeline --name Impulses_script
