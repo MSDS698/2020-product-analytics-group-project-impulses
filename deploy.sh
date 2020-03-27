f3c7ba7f3076c77634f6edfae11efb3ceb2079cd
esme <esmeluo@gmail.com>
2020-03-27 00:44:21 -0700

    terminate env/delete app/delete codepipeline

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,5 @@ echo "3\n\nImpulses_script\nY\n1\nn\nn\n" | eb init -i
 eb create imp-env-test
 aws codepipeline create-pipeline --cli-input-json file://pipeline.json
 eb terminate
-#aws elasticbeanstalk delete-application --application-name Impulses_script
+aws elasticbeanstalk delete-application --application-name Impulses_script
+aws codepipeline delete-pipeline --name Impulses_script
