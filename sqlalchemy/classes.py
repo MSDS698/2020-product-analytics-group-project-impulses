c1006aa60aa2ceffb5f524d7cb068f5dd403e16e
Zhentao Hou <zhou2@dons.usfca.edu>
2020-04-05 11:26:01 -0700

    add foreign keys

--- sqlalchemy/classes.py
+++ sqlalchemy/classes.py
@@ -16,8 +16,8 @@ class User(db.Model):
     auth_id: unique user id from OAuth; string
     first_name: user's first name; string
     last_name: user's last name; string
-    email: user's email address; string
-    phone: user's phone number; bigint
+    email: user's email address; string; unique
+    phone: user's phone number; bigint; unique
     signup_date: user's signup date; date
     status: user's current status; string
     """
@@ -27,22 +27,16 @@ class User(db.Model):
     auth_id = db.Column("auth_id", db.String, nullable=False)
     first_name = db.Column("first_name", db.String, nullable=False)
     last_name = db.Column("last_name", db.String, nullable=False)
-    email = db.Column("email", db.String, nullable=False)
-    phone = db.Column("phone", db.BigInteger, nullable=False)
+    email = db.Column("email", db.String, unique=True, nullable=False)
+    phone = db.Column("phone", db.BigInteger, unique=True, nullable=False)
     signup_date = db.Column("signup_date", db.Date, nullable=False)
     status = db.Column("status", db.String, nullable=False)
-
-    def __init__(self, user_id, auth_id, first_name, last_name,
-                 email, phone, signup_date, status):
-        """Initializes User class"""
-        self.user_id = user_id
-        self.auth_id = auth_id
-        self.first_name = first_name
-        self.last_name = last_name
-        self.email = email
-        self.phone = phone
-        self.signup_date = signup_date
-        self.status = status
+    # relationships
+    password = db.relationship("Password", backref="user")
+    plaid_items = db.relationship("PlaidItems", backref="user")
+    accounts = db.relationship("Accounts", backref="user")
+    transaction = db.relationship("Transaction", backref="user")
+    savings_history = db.relationship("SavingsHistory", backref="user")
 
 
 class Password(db.Model):
@@ -56,14 +50,9 @@ class Password(db.Model):
     __table_args__ = {"schema": "dw"}
     __tablename__ = "password"
     password_id = db.Column("password_id", db.BigInteger, primary_key=True)
-    user_id = db.Column("user_id", db.BigInteger, nullable=False)
+    user_id = db.Column("user_id", db.BigInteger,
+                        db.ForeignKey("dw.user.user_id"))
     password = db.Column("password", db.String, nullable=False)
-
-    def __init__(self, password_id, user_id, password):
-        """Initializes Password class"""
-        self.password_id = password_id
-        self.user_id = user_id
-        self.password = password
 
 
 class PlaidItems(db.Model):
@@ -79,16 +68,12 @@ class PlaidItems(db.Model):
     __table_args__ = {"schema": "dw"}
     __tablename__ = "plaid_items"
     plaid_id = db.Column("plaid_id", db.BigInteger, primary_key=True)
-    user_id = db.Column("user_id", db.BigInteger, nullable=False)
+    user_id = db.Column("user_id", db.BigInteger,
+                        db.ForeignKey("dw.user.user_id"))
     item_id = db.Column("item_id", db.String, nullable=False)
     access_token = db.Column("access_token", db.String, nullable=False)
-
-    def __init__(self, plaid_id, user_id, item_id, access_token):
-        """Initializes PlaidItems class"""
-        self.plaid_id = plaid_id
-        self.user_id = user_id
-        self.item_id = item_id
-        self.access_token = access_token
+    # relationships
+    accounts = db.relationship("Accounts", backref="plaid_item")
 
 
 class Accounts(db.Model):
@@ -106,24 +91,17 @@ class Accounts(db.Model):
     __table_args__ = {"schema": "dw"}
     __tablename__ = "accounts"
     account_id = db.Column("account_id", db.BigInteger, primary_key=True)
-    user_id = db.Column("user_id", db.BigInteger, nullable=False)
-    plaid_id = db.Column("plaid_id", db.BigInteger, nullable=False)
+    user_id = db.Column("user_id", db.BigInteger,
+                        db.ForeignKey("dw.user.user_id"))
+    plaid_id = db.Column("plaid_id", db.BigInteger,
+                         db.ForeignKey("dw.plaid_items.plaid_id"))
     account_plaid_id = db.Column("account_plaid_id", db.String,
                                  nullable=False)
     account_name = db.Column("account_name", db.String)
     account_type = db.Column("account_type", db.String)
     account_subtype = db.Column("account_subtype", db.String)
-
-    def __init__(self, account_id, user_id, plaid_id, account_plaid_id,
-                 account_name, account_type, account_subtype):
-        """Initializes Accounts class"""
-        self.account_id = account_id
-        self.user_id = user_id
-        self.plaid_id = plaid_id
-        self.account_plaid_id = account_plaid_id
-        self.account_name = account_name
-        self.account_type = account_type
-        self.account_subtype = account_subtype
+    # relationships
+    transaction = db.relationship("Transaction", backref="account")
 
 
 class Transaction(db.Model):
@@ -152,8 +130,10 @@ class Transaction(db.Model):
     __tablename__ = "transaction"
     transaction_id = db.Column("transaction_id", db.BigInteger,
                                primary_key=True)
-    user_id = db.Column("user_id", db.BigInteger, nullable=False)
-    account_id = db.Column("account_id", db.BigInteger, nullable=False)
+    user_id = db.Column("user_id", db.BigInteger,
+                        db.ForeignKey("dw.user.user_id"))
+    account_id = db.Column("account_id", db.BigInteger,
+                           db.ForeignKey("dw.accounts.account_id"))
     trans_amount = db.Column("trans_amount", db.Numeric(10, 2),
                              nullable=False)
     category_id = db.Column("category_id", db.BigInteger, nullable=False)
@@ -168,29 +148,6 @@ class Transaction(db.Model):
     merchant_postal_code = db.Column("merchant_postal_code", db.String)
     merchant_longitude = db.Column("merchant_longitude", db.String)
     merchant_latitude = db.Column("merchant_latitude", db.String)
-
-    def __init__(self, transaction_id, user_id, account_id, trans_amount,
-                 category_id, is_preferred_saving, trans_date, post_date,
-                 merchant_category, merchant_address, merchant_city,
-                 merchant_state, merchant_country, merchant_postal_code,
-                 merchant_longitude, merchant_latitude):
-        """Initializes Transaction class"""
-        self.transaction_id = transaction_id
-        self.user_id = user_id
-        self.account_id = account_id
-        self.trans_amount = trans_amount
-        self.category_id = category_id
-        self.is_preferred_saving = is_preferred_saving
-        self.trans_date = trans_date
-        self.post_date = post_date
-        self.merchant_category = merchant_category
-        self.merchant_address = merchant_address
-        self.merchant_city = merchant_city
-        self.merchant_state = merchant_state
-        self.merchant_country = merchant_country
-        self.merchant_postal_code = merchant_postal_code
-        self.merchant_longitude = merchant_longitude
-        self.merchant_latitude = merchant_latitude
 
 
 class SavingsHistory(db.Model):
@@ -209,7 +166,8 @@ class SavingsHistory(db.Model):
     __table_args__ = {"schema": "dw"}
     __tablename__ = "savings_history"
     savings_id = db.Column("savings_id", db.BigInteger, primary_key=True)
-    user_id = db.Column("user_id", db.BigInteger, nullable=False)
+    user_id = db.Column("user_id", db.BigInteger,
+                        db.ForeignKey("dw.user.user_id"))
     savings_amount = db.Column("savings_amount", db.Numeric(10, 2),
                                nullable=False)
     total_savings = db.Column("total_savings", db.Numeric(10, 2),
@@ -217,17 +175,6 @@ class SavingsHistory(db.Model):
     predicted_savings = db.Column("predicted_savings", db.Numeric(10, 2))
     transfer_date = db.Column("transfer_date", db.Date, nullable=False)
     update_date = db.Column("update_date", db.Date, nullable=False)
-
-    def __init__(self, savings_id, user_id, savings_amount, total_savings,
-                 predicted_savings, transfer_date, update_date):
-        """Initializes SavingsHistory class"""
-        self.savings_id = savings_id
-        self.user_id = user_id
-        self.savings_amount = savings_amount
-        self.total_savings = total_savings
-        self.predicted_savings = predicted_savings
-        self.transfer_date = transfer_date
-        self.update_date = update_date
 
 
 db.create_all()
