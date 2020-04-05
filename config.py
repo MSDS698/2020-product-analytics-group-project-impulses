0297638f53d2e924feedf4e6e7a61fc0c9917a28
Kevin Loftis <loftiskg@gmail.com>
2020-04-04 19:04:44 -0700

    added prod,dev,test databases

--- config.py
+++ config.py
@@ -3,6 +3,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'test.db')
+    # local testing
+    #SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'test.db')
+    # development
+    SQLALCHEMY_DATABASE_URI = 'postgresql://masteruser:productimpulses@maindb.cuwtgivgs05r.us-west-1.rds.amazonaws.com/impulses_dev'
+    # # production
+    # SQLALCHEMY_DATABASE_URI = 'postgresql://masteruser:productimpulses@maindb.cuwtgivgs05r.us-west-1.rds.amazonaws.com/impulses_prod'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SECRET_KEY = os.urandom(24)
