40811d154b0efd4e30ab3357f55cab8e06812a26
Kevin Loftis <loftiskg@gmail.com>
2020-04-09 12:20:39 -0700

    made test_setup class

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,35 +1,37 @@
 import os
 import unittest
 import flask
+from test_setup import TestSetup
+from app import classes, db
 
-TEST_DB = 'test.db'
-BASEDIR = os.path.abspath(os.path.dirname(__file__))
-os.environ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
-    os.path.join(BASEDIR, TEST_DB)
+# TEST_DB = 'test.db'
+# BASEDIR = os.path.abspath(os.path.dirname(__file__))
+# os.environ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+#     os.path.join(BASEDIR, TEST_DB)
 
-from app import application, classes, db
+# from app import application, classes, db
 
 
  
 
 
  
-class TestSetup(unittest.TestCase):
+class TestRoutes(TestSetup):
 
-    # executed prior to each test
-    def setUp(self):
-        application.config['TESTING'] = True
-        application.config['WTF_CSRF_ENABLED'] = False
-        application.config['DEBUG'] = False
-        application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
-            os.path.join(BASEDIR, TEST_DB)
-        self.app = application.test_client()
-        db.drop_all()
-        db.create_all()
+    # # executed prior to each test
+    # def setUp(self):
+    #     application.config['TESTING'] = True
+    #     application.config['WTF_CSRF_ENABLED'] = False
+    #     application.config['DEBUG'] = False
+    #     application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+    #         os.path.join(BASEDIR, TEST_DB)
+    #     self.app = application.test_client()
+    #     db.drop_all()
+    #     db.create_all()
  
-    # executed after each test
-    def tearDown(self):
-        os.remove(os.path.join(BASEDIR,TEST_DB))
+    # # executed after each test
+    # def tearDown(self):
+    #     os.remove(os.path.join(BASEDIR,TEST_DB))
  
  
 ###############
