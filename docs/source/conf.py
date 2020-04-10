fa9407982a2a5f8bcc201002662da07b5afdeb96
Esther Liu <yl3374@nyu.edu>
2020-04-10 10:16:22 -0700

    fix pep8 formatting

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,6 @@ sys.path.insert(0, os.path.abspath("../../"))
 sys.path.insert(0, os.path.abspath("../../tests"))
 
 
-
 # -- Project information -----------------------------------------------------
 
 project = "Impulses"
