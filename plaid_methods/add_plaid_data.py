90c09e4186598d93215544cc677f89869e2f3bec
Lea Genuit <lea@Leas-MacBook-Pro.local>
2020-04-09 18:24:01 -0700

    Refactor code to ORM SQLAlchemy

--- plaid_methods/add_plaid_data.py
+++ plaid_methods/add_plaid_data.py
@@ -1,81 +1,55 @@
-import ast
-from sqlalchemy import MetaData, select
+from app import db, classes
 
 
-def parse_plaid_data(plaid_data):
-    """
-    Parse a string from plaid into python object
-    :param plaid_data: name of the file (str)
-    :return: list of dictionary
-    """
-    plaid_data = plaid_data.replace('\n', '')
-    data = ast.literal_eval(plaid_data)
-    return data
-
-
-def add_accounts(db, accounts_data, user_id):
+def add_accounts(accounts, user, plaid_item, commit=True):
     """
     Add account information to the database
-    :param db: database connection
-    :param accounts_data: data with account information from Plaid (str)
-    :param user_id: user id associated to the account (int)
+    :param accounts: accounts data from plaid api
+    :param user: User SQLAlchemy object of the user associated with the accounts
+    :param plaid_item: PlaidItem SQLalchemy object of the plaid_item associated with
+    the accounts
+    :param commit: If commit is True then commits transactions the add to the database
     """
-    accounts = parse_plaid_data(accounts_data)
 
-    meta = MetaData(bind=db, reflect=True)
-    with db.connect() as conn:
-        select_statement = select([meta.tables['dw.plaid_items'].c.plaid_item_id]) \
-            .where(meta.tables['dw.plaid_items'].c.user_id == user_id)
-        result_set = conn.execute(select_statement)
-        for r in result_set:
-            plaid_id = r[0]
-        for account in accounts:
-            insert_statement = meta.tables['dw.accounts']. \
-                insert().values(user_id=user_id, plaid_id=plaid_id,
-                                account_plaid_id=account['account_id'],
-                                account_name=account['name'],
-                                account_type=account['type'],
-                                account_subtype=account['subtype'])
-            conn.execute(insert_statement)
+    for account in accounts:
+        acc = classes.Accounts(account_plaid_id=account['account_id'],
+                               account_name=account['name'],
+                               account_type=account['type'],
+                               account_subtype=account['subtype'],
+                               user_id=user.user_id,
+                               plaid_id=plaid_item.plaid_item_id)
+        db.session.add(acc)
+    if commit is True:
+        db.session.commit()
 
 
-def add_transactions(db, transactions_data):
+def add_transactions(transactions, user, account, commit=True):
     """
     Add account information to the database
-    :param db: database connection
-    :param transactions_data: data with transactions information
-    from Plaid (str)
+    :param transactions: transactions data from plaid api
+    :param user: User SQLAlchemy object of the user associated with the transactions
+    :param account: Accounts SQLAlchemy object of the user associated with the transactions
+    :param commit: If commit is True then commits transactions the add to the database
     """
-    transactions = parse_plaid_data(transactions_data)
 
-    meta = MetaData(bind=db, reflect=True)
-    with db.connect() as conn:
-        for transaction in transactions:
-            select_statement = select(
-                [meta.tables['dw.accounts'].c.user_id,
-                 meta.tables['dw.accounts']
-                     .c.account_id]).where(meta.tables['dw.accounts'].
-                                           c.account_plaid_id ==
-                                           transaction['account_id'])
-            result_set = conn.execute(select_statement)
-            for r in result_set:
-                user_id = r[0]
-                account_id = r[1]
-            loc = transaction['location']
-            categories = ';'.join(transaction['category'])
-            insert_statement = meta.tables['dw.transaction']. \
-                insert().values(user_id=user_id,
-                                account_id=account_id,
-                                trans_date=transaction['date'],
-                                post_date=transaction['authorized_date'],
-                                trans_amount=transaction['amount'],
-                                merchant_category=categories,
-                                merchant_address=loc['address'],
-                                merchant_city=loc['city'],
-                                merchant_state=loc['region'],
-                                merchant_country=loc['country'],
-                                merchant_postal_code=loc['postal_code'],
-                                merchant_longitude=loc['lon'],
-                                merchant_latitude=loc['lat'],
-                                category_id=transaction['category_id'])
-            conn.execute(insert_statement)
+    for transaction in transactions:
+        loc = transaction['location']
+        categories = ';'.join(transaction['category'])
+        trans = classes.Transaction(user_id=user.user_id,
+                                    account_id=account.account_id,
+                                    trans_date=transaction['date'],
+                                    post_date=transaction['authorized_date'],
+                                    trans_amount=transaction['amount'],
+                                    merchant_category=categories,
+                                    merchant_address=loc['address'],
+                                    merchant_city=loc['city'],
+                                    merchant_state=loc['region'],
+                                    merchant_country=loc['country'],
+                                    merchant_postal_code=loc['postal_code'],
+                                    merchant_longitude=loc['lon'],
+                                    merchant_latitude=loc['lat'],
+                                    category_id=transaction['category_id']
+                                    )
+        db.session.add(trans)
+    if commit is True:
+        db.session.commit()
