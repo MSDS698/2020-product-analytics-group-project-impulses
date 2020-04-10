85f4edaf837914c7608051a6630a5028db47e235
leagenuit <52584143+leagenuit@users.noreply.github.com>
2020-04-09 20:47:05 -0700

    Add plaid data (#28)
    * Methods to add accounts information and transactions data to the database
    
    * Move file to directory plaid_methods
    
    * Parse string instead of a file
    
    * Refactor code
    
    * Refactor code to ORM SQLAlchemy
    
    * Resolve Python Style Checker
    
    * Resolve Python Style Checker
    
    Co-authored-by: Lea Genuit <lea@Leas-MacBook-Pro.local>

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Impulses", # Replace with your own username
+    name="Impulses",  # Replace with your own username
     version="0.0.1",
     author="Kevin Loftis",
     author_email="loftiskg@gmail.com",
