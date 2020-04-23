939cd4377c8f3d0d31626c68c581b8a62f610d31
esme <esmeluo@gmail.com>
2020-04-23 09:45:10 -0700

    update dow function and change cron job config to master branch

--- twilio_methods/schedule_methods.py
+++ twilio_methods/schedule_methods.py
@@ -16,13 +16,7 @@ def dow_list(dow: str):
     :type [dow]: [string]
     """
 
-    if len(dow) == 1 and dow != "*":
-        return [int(dow)]
-    elif "-" in dow:
-        return [days for days in range(int(dow[0]), int(dow[-1])+1)]
-    elif "," in dow:
-        return list(map(int, dow.split(",")))
-    elif dow == "*" or dow.lower() == "everyday":
+    if dow.lower() == "everyday":
         return [days for days in range(7)]
     elif dow.lower() == "weekday":
         return [days for days in range(5)]
@@ -46,7 +40,7 @@ def habit_today(df_habit, df_user):
 
     df_today = df_habit.loc[(df_habit["time_hour"] >= now.hour) &
                             (df_habit["time_minute"] >= now.minute) &
-                            [now.weekday()+1 in lst
+                            [now.weekday() in lst
                             for lst in
                             df_habit.time_day_of_week.apply(dow_list)]]
 
