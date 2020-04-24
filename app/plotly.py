6e5bbb61630d4ffa26ea3e09a830fbbb719da4d4
Esther Liu <yl3374@nyu.edu>
2020-04-24 00:40:49 -0500

    fix pep8

--- app/plotly.py
+++ app/plotly.py
@@ -8,7 +8,6 @@ def plotly_saving_history(saving_date, saving_coins):
     saving_coins_sum = [saving_coins[0][0]]
     for i, coin in enumerate(saving_coins[1:]):
         saving_coins_sum.append(saving_coins_sum[i] + coin[0])
-        
     df = pd.DataFrame(saving_date, saving_coins_sum).reset_index()
     df.columns = ['coins', 'date']
 
@@ -18,9 +17,9 @@ def plotly_saving_history(saving_date, saving_coins):
     return output
 
 
-def plotly_percent_saved(num_saved,num_total_suggestions):
-    labels = ['Saved','Unsaved']
-    values = [num_saved,num_total_suggestions-num_saved]
+def plotly_percent_saved(num_saved, num_total_suggestions):
+    labels = ['Saved', 'Unsaved']
+    values = [num_saved, num_total_suggestions - num_saved]
 
     # pull is given as a fraction of the pie radius
     fig = go.Figure(data=[go.Pie(labels=labels, values=values, pull=[0.2, 0])])
