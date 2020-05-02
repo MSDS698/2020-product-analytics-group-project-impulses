49535cb9ba54c9566c246eb9650887927669b5ab
Zhentao Hou <52581437+usfzhou2@users.noreply.github.com>
2020-05-01 20:42:19 -0700

    Add lottery drawing (#57)
    * add lottery drawing function
    
    * add entries to user_lottery_log table
    
    * update flask migrations
    
    * fix timezone issues
    
    * fix timezone issues
    
    * add error handler 401
    
    * move lottery drawing and send message to winner
    
    * add more error handlers
    
    * fix lottery drawing user entries
    
    * update link in cron to test lottery drawing
    
    * update cron for merging to master
    
    * delete .idea folder
    
    Co-authored-by: esme <esmeluo@gmail.com>

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
 import os
+import random
 from datetime import datetime
 from app import application, classes, db
 from flask import redirect, render_template, url_for, request, flash
@@ -93,17 +94,59 @@ def enter_lottery(user, lottery):
     user has.
 
     A new coin transaction will be added to coin table and the coins column
-    in user table will also be updated. A new lottery log will be added to
-    user_lottery_log table.
+    in user table will also be updated. The user_lottery_log table will also
+    be updated.
     """
+    # check if the user has bought the lottery before
+    lottery_log = classes.UserLotteryLog.query.filter_by(
+        user=user, lottery=lottery).first()
+    if lottery_log:
+        lottery_log.entries += 1
+    else:  # the user buys the lottery for the first time
+        new_lottery_log = classes.UserLotteryLog(user=user, lottery=lottery)
+        db.session.add(new_lottery_log)
+
+    # update the user's coins
     tz = pytz.timezone("America/Los_Angeles")
     new_coin = classes.Coin(user=user, coin_amount=-lottery.cost,
                             log_date=datetime.now().astimezone(tz).date(),
                             description="lottery")
-    new_lottery_log = classes.UserLotteryLog(user=user, lottery=lottery)
     user.coins -= lottery.cost
     db.session.add(new_coin)
-    db.session.add(new_lottery_log)
+    db.session.commit()
+
+
+# lottery drawing function
+def lottery_drawing():
+    """Draw the winner for lotteries that have ended.
+
+    First check which lotteries have ended without the winner drawn.
+    Then choose the winner, send message to the winner, and update
+    the lottery table.
+    """
+    tz = pytz.timezone("America/Los_Angeles")
+    current_time = datetime.now().astimezone(tz)
+    lottery_to_draw = classes.Lottery.query.filter(
+        classes.Lottery.winner_user_id.is_(None),
+        classes.Lottery.end_date <= str(current_time)).all()
+
+    for lottery in lottery_to_draw:
+        participants = classes.UserLotteryLog.query.filter_by(
+            lottery=lottery).all()
+        participants = [p.user_id for p in participants
+                        for _ in range(p.entries)]
+
+        if participants:
+            lottery.winner_user_id = winner = random.choice(participants)
+            # send message to the winner
+            body = f"Congratulations! You've won the lottery for " \
+                   + f"{lottery.lottery_name}!"
+            twilio_client.messages.create(
+                body=body,
+                to=classes.User.query.filter_by(id=winner).first().phone,
+                from_="+16462573594")
+        else:
+            lottery.winner_user_id = -1
     db.session.commit()
 
 
@@ -150,9 +193,9 @@ def register():
         ) + classes.User.query.filter_by(phone=phone).count())
 
         if len(phone) != 10:
-            flash('Error - Please enter a valid phone number')
+            flash('Please enter a valid phone number')
         elif user_count != 0:
-            flash('Error - User already exists')
+            flash('User already exists')
         else:
             # User information does not already exist in DB
             user = classes.User(first_name, last_name, email, phone, password)
@@ -340,6 +383,26 @@ def logout():
     return redirect(url_for("index"))
 
 
+@application.errorhandler(401)
+def re_route(e):
+    return redirect(url_for("login"))
+
+
+@application.errorhandler(403)
+def re_route(e):
+    return redirect(url_for("index"))
+
+
+@application.errorhandler(404)
+def re_route(e):
+    return redirect(url_for("index"))
+
+
+@application.errorhandler(500)
+def re_route(e):
+    return redirect(url_for("index"))
+
+
 @application.route("/access_plaid_token", methods=["POST", "GET"])
 def access_plaid_token():
     try:
@@ -347,11 +410,11 @@ def access_plaid_token():
         # extract selected account information from response
         selected_accounts_data = [
             key for key in request.form.keys() if key.startswith('accounts')]
-        account_indicies = set([int(field[9])
-                                for field in selected_accounts_data])
+        account_indices = set([int(field[9])
+                               for field in selected_accounts_data])
         accounts = []
 
-        for idx in account_indicies:
+        for idx in account_indices:
             accounts.append(
                 {'account_id': request.form[f'accounts[{idx}][id]'],
                  'name': request.form[f'accounts[{idx}][name]'],
@@ -410,10 +473,13 @@ def send_message():
                 habit.time_hour == now.hour:
             body = f"Would you like to save $5 on {habit.habit_category} " + \
                    "today? Respond Y/N"
-            msg = twilio_client.messages.create(
+            twilio_client.messages.create(
                 body=body,
                 to=habit.user.phone,
                 from_="+16462573594")
+
+    # lottery drawing and send message to the winner
+    lottery_drawing()
 
     return redirect(url_for("index"))
 
