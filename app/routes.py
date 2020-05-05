7a33851031f1a8905d9a611f5ba0fdb68a2c52e5
andrewhowcool <andrewcyoung1@gmail.com>
2020-05-04 22:43:57 -0700

    fix python style

--- app/routes.py
+++ app/routes.py
@@ -19,6 +19,7 @@ from app.plotly_dashboard import plotly_saving_history, plotly_percent_saved,\
     select_past_week
 from scripts.extract_habit import Insights
 
+
 ENV_VARS = {
     "PLAID_CLIENT_ID": os.environ["PLAID_CLIENT_ID"],
     "PLAID_PUBLIC_KEY": os.environ["PLAID_PUBLIC_KEY"],
@@ -182,7 +183,8 @@ def habit_table_save_changes():
         for i in range(len(habit_name)):
             habit = classes.Habits(user_id=user_id, habit_name=habit_name[i],
                                    habit_category=habit_category[i],
-                                   time_minute=time_hour_minute[i].split(':')[1],
+                                   time_minute=time_hour_minute[i]
+                                   .split(':')[1],
                                    time_hour=time_hour_minute[i].split(':')[0],
                                    time_day_of_week=time_day_of_week[i])
             db.session.add(habit)
@@ -310,7 +312,7 @@ def dashboard():
     saving_percent_plot = plotly_percent_saved(num_saved,
                                                num_total_suggestions)
     # Retrieve spending habits for Insights
-    categories_file = os.path.join(os.getcwd(), 'scripts', 'categories.json')
+    categories_file = os.path.join(os.getcwd(), 'Scripts', 'categories.json')
 
     beginning_month = datetime(year=datetime.now().year,
                                month=datetime.now().month,
