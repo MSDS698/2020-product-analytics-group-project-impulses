0873e78506c2c04d0c477f421d2c53cfce02e3c2
Zhentao Hou <52581437+usfzhou2@users.noreply.github.com>
2020-05-05 15:07:58 -0700

    Add coin transaction hover (#62)
    * add coin popover
    
    * add hover state
    
    * move helper functions to a separate file
    
    * change first time login desc to registration
    
    * small fix

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,4 @@
 import os
-import random
 from datetime import datetime
 from app import application, classes, db
 from flask import redirect, render_template, url_for, request, flash
@@ -14,6 +13,8 @@ import pytz
 import pandas as pd
 import twilio.rest
 from twilio.twiml.messaging_response import MessagingResponse
+from scripts.coin_transaction import add_login_coin, add_saving_coin, \
+    enter_lottery, lottery_drawing
 from app.plotly_dashboard import plotly_saving_history, plotly_percent_saved
 from scripts.extract_habit import Insights
 
@@ -39,116 +40,6 @@ client = Client(
 twilio_client = twilio.rest.Client(
     ENV_VARS["TWILIO_ACCOUNT_SID"],
     ENV_VARS["TWILIO_AUTH_TOKEN"])
-
-
-# helper function to update user coins when logging in
-def add_login_coin(user):
-    """Update user coins when logging in.
-
-    When the user is logged in for the first time, 10 coins will be added
-    as a sign-up bonus. For regular user login, 2 coins are rewarded daily.
-
-    If any changes occur, a new coin transaction will be added to coin
-    table and the coins column in user table will also be updated.
-    """
-    login_coin_date = db.session.query(db.func.max(classes.Coin.log_date)) \
-        .filter_by(user=user, description="login").scalar()
-    tz = pytz.timezone("America/Los_Angeles")
-    if login_coin_date is None:  # first time login
-        coin_amount = 10
-    elif (datetime.now().astimezone(tz).date() - login_coin_date).days > 0:
-        # daily login
-        coin_amount = 2
-    else:
-        return
-    new_coin = classes.Coin(user=user, coin_amount=coin_amount,
-                            log_date=datetime.now().astimezone(tz).date(),
-                            description="login")
-    user.coins += coin_amount
-    db.session.add(new_coin)
-    db.session.commit()
-
-
-# helper function to update user coins when replying "yes" to saving texts
-def add_saving_coin(user):
-    """Update user coins when replying "yes" to saving texts.
-
-    When the user replies "yes" to saving text messages, 10 coins will be
-    added. A new coin transaction will be added to coin table and the coins
-    column in user table will also be updated.
-    """
-    tz = pytz.timezone("America/Los_Angeles")
-    new_coin = classes.Coin(user=user, coin_amount=10,
-                            log_date=datetime.now().astimezone(tz).date(),
-                            description="saving")
-    user.coins += 10
-    db.session.add(new_coin)
-    db.session.commit()
-
-
-# helper function to update user coins when entering a lottery
-def enter_lottery(user, lottery):
-    """Update user coins when buying an entry to a lottery.
-
-    When the user buys an entry to a lottery, coins corresponding to the
-    lottery cost will be deducted from the total number of coins that the
-    user has.
-
-    A new coin transaction will be added to coin table and the coins column
-    in user table will also be updated. The user_lottery_log table will also
-    be updated.
-    """
-    # check if the user has bought the lottery before
-    lottery_log = classes.UserLotteryLog.query.filter_by(
-        user=user, lottery=lottery).first()
-    if lottery_log:
-        lottery_log.entries += 1
-    else:  # the user buys the lottery for the first time
-        new_lottery_log = classes.UserLotteryLog(user=user, lottery=lottery)
-        db.session.add(new_lottery_log)
-
-    # update the user's coins
-    tz = pytz.timezone("America/Los_Angeles")
-    new_coin = classes.Coin(user=user, coin_amount=-lottery.cost,
-                            log_date=datetime.now().astimezone(tz).date(),
-                            description="lottery")
-    user.coins -= lottery.cost
-    db.session.add(new_coin)
-    db.session.commit()
-
-
-# lottery drawing function
-def lottery_drawing():
-    """Draw the winner for lotteries that have ended.
-
-    First check which lotteries have ended without the winner drawn.
-    Then choose the winner, send message to the winner, and update
-    the lottery table.
-    """
-    tz = pytz.timezone("America/Los_Angeles")
-    current_time = datetime.now().astimezone(tz)
-    lottery_to_draw = classes.Lottery.query.filter(
-        classes.Lottery.winner_user_id.is_(None),
-        classes.Lottery.end_date <= str(current_time)).all()
-
-    for lottery in lottery_to_draw:
-        participants = classes.UserLotteryLog.query.filter_by(
-            lottery=lottery).all()
-        participants = [p.user_id for p in participants
-                        for _ in range(p.entries)]
-
-        if participants:
-            lottery.winner_user_id = winner = random.choice(participants)
-            # send message to the winner
-            body = f"Congratulations! You've won the lottery for " \
-                   + f"{lottery.lottery_name}!"
-            twilio_client.messages.create(
-                body=body,
-                to=classes.User.query.filter_by(id=winner).first().phone,
-                from_="+16462573594")
-        else:
-            lottery.winner_user_id = -1
-    db.session.commit()
 
 
 @application.route("/index")
@@ -323,11 +214,13 @@ def dashboard():
 
     # using the login coins now for demo, since we don't have enough data for
     # the saving coins yet
-    saving_date = classes.Coin.query.filter_by(user_id=user_id,
-                                               description='login') \
+    saving_date = classes.Coin.query.filter(
+        classes.Coin.user_id == user_id,
+        classes.Coin.description.in_(['login', 'registration'])) \
         .with_entities(classes.Coin.log_date).all()
-    saving_coins = classes.Coin.query.filter_by(user_id=user_id,
-                                                description='login') \
+    saving_coins = classes.Coin.query.filter(
+        classes.Coin.user_id == user_id,
+        classes.Coin.description.in_(['login', 'registration'])) \
         .with_entities(classes.Coin.coin_amount).all()
     savings_bar_plot, total_saving_coins = \
         plotly_saving_history(saving_date, saving_coins)
@@ -362,8 +255,13 @@ def dashboard():
                             habit_name, thresholds[ind])
         if insights.transactions is not None:
             insights_list.append(insights)
+
+    coin_log = classes.Coin.query.filter_by(user=current_user).order_by(
+        classes.Coin.id.desc()).limit(6).all()
+
     return render_template("dashboard.html",
                            user=current_user,
+                           coin_log=coin_log,
                            form=classes.HabitForm(),
                            lottery_status=lottery_status,
                            available_lottery_records=available_lottery_records,
