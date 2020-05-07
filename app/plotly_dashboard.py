15809dfd54ebcc91dde911645cd431f3a76e764b
Esther Liu <yl3374@nyu.edu>
2020-05-06 23:40:11 -0500

    save bug for first time user w/o savings

--- app/plotly_dashboard.py
+++ app/plotly_dashboard.py
@@ -5,19 +5,22 @@ import pandas as pd
 
 
 def plotly_saving_history(saving_date, saving_coins):
-    saving_coins_sum = [saving_coins[0][0]]
-    for i, coin in enumerate(saving_coins[1:]):
-        saving_coins_sum.append(saving_coins_sum[i] + coin[0])
-    df = pd.DataFrame(saving_date, saving_coins_sum).reset_index()
-    df.columns = ['coins', 'date']
-    fig = px.line(df, x="date", y="coins")
-    fig.update_layout(xaxis_title=None,
-                      paper_bgcolor='rgba(0,0,0,0)',
-                      plot_bgcolor='rgba(0,0,0,0)')
-    fig.update_yaxes(showgrid=True, gridwidth=1, gridcolor='LightGrey')
-    output = plotly.offline.plot(fig, include_plotlyjs=False,
-                                 output_type='div')
-    return output, saving_coins_sum[-1]
+    if len(saving_coins) != 0: 
+        saving_coins_sum = [saving_coins[0][0]]
+        for i, coin in enumerate(saving_coins[1:]):
+            saving_coins_sum.append(saving_coins_sum[i] + coin[0])
+        df = pd.DataFrame(saving_date, saving_coins_sum).reset_index()
+        df.columns = ['coins', 'date']
+        fig = px.line(df, x="date", y="coins")
+        fig.update_layout(xaxis_title=None,
+                          paper_bgcolor='rgba(0,0,0,0)',
+                          plot_bgcolor='rgba(0,0,0,0)')
+        fig.update_yaxes(showgrid=True, gridwidth=1, gridcolor='LightGrey')
+        output = plotly.offline.plot(fig, include_plotlyjs=False,
+                                     output_type='div')
+        return output, saving_coins_sum[-1]
+    else:
+        return 'No savings yet', 0
 
 
 def plotly_percent_saved(num_saved, num_total_suggestions):
