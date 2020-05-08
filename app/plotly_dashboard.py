245a7d4aa602856498260c05fd4c7ea565cf29bb
Esther Liu <yl3374@nyu.edu>
2020-05-07 19:43:53 -0500

    delete text for user w/o saving

--- app/plotly_dashboard.py
+++ app/plotly_dashboard.py
@@ -19,18 +19,28 @@ def select_past_week(saving_date):
     if last_week_cnt == 0:
         percentage = 0
     else:
-        percentage = round((this_week_cnt - last_ween_cnt) / 
-                           last_ween_cnt * 100)
+        percentage = round((this_week_cnt - last_week_cnt) / 
+                           last_week_cnt * 100)
     return percentage, this_week_cnt * 10
 
 
 def plotly_saving_history(saving_date, saving_coins):
     if len(saving_coins) != 0:
-        saving_coins_sum = [saving_coins[0][0]]
+        saving_coins_sum = [(saving_date[0][0], saving_coins[0][0])]
         for i, coin in enumerate(saving_coins[1:]):
-            saving_coins_sum.append(saving_coins_sum[i] + coin[0])
-        df = pd.DataFrame(saving_date, saving_coins_sum).reset_index()
-        df.columns = ['coins', 'date']
+            saving_coins_sum.append(
+                (saving_date[i + 1][0], saving_coins_sum[i][1] + coin[0]))
+
+        saving_dict = dict(saving_coins_sum)
+        base = datetime.today().date()
+        latest_date = saving_coins_sum[-1][0]
+        date_list = [(base - timedelta(days=x)) for x in
+                     range((base - latest_date).days, -1, -1)]
+        for dates in date_list:
+            saving_dict[dates] = saving_dict[latest_date]
+        df = pd.DataFrame(saving_dict.items())
+        df.columns = ['date', 'coins']
+
         fig = px.line(df, x="date", y="coins")
         fig.update_layout(
             xaxis_range=[datetime.now().astimezone(tz) - timedelta(days=7),
